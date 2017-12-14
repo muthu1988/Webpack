c7f5ab7c6ea7fab6aea6a20659391ca8d02432f0
Muthu Kumaran <muthukumaran.a@nationstarmail.com>
2017-12-14 09:35:06 +0530

    Loaders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
-
+import './style.css';
+import Butterfly from './butterfly.png';
 function component() {
     var element = document.createElement('div');  
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    var myIcon = new Image();
+    myIcon.src = Butterfly;
+    element.appendChild(myIcon);
     return element;
   }
   
