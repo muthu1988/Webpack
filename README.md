f71e1f54d410b1372e6ad510084bcccee5937e30
Muthu Kumaran <muthukumaran.a@nationstarmail.com>
2017-12-16 13:16:01 +0530

    Basic Concepts

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Webpack
-
-Webpack Features and Concepts
+Webpack Features and Concept
+    Concepts - Entry, Output, Plugin, Loader
+    Loaders - CSS Loader, File Loader
+    Plugins - CleanWebpackPlugin, HtmlWebpackPlugin
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
 import './style.css';
 import Butterfly from './butterfly.png';
+import printMe from './print.js';
 function component() {
-    var element = document.createElement('div');  
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
     element.classList.add('hello');
+    element.appendChild(btn);
     var myIcon = new Image();
     myIcon.src = Butterfly;
     element.appendChild(myIcon);
